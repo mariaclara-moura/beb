bb6c3d284fe19cf2e91ac13479158e36dd4a5880
Maria Clara Albuquerque Moura <mcam3@cin.ufpe.br>
2024-01-18 18:00:45 -0300

    improve home photo quality

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -2,7 +2,7 @@ import Icon from "./icon-2.png";
 import WhiteIcon from "./white-icon.svg";
 import Infraestrutura from "./Group 62.svg"
 import Predial from "./Group 52.svg"
-import Canos from "./canes.svg"
+import Canos from "./canes.jpg"
 import Insta from "./instagram.svg";
 import Zap from "./zap.svg";
 import Map from "./map.svg";
